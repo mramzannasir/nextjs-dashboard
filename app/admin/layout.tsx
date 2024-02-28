40f8d0c3e6fb65f15e5d6f034e01e758cd083930
mramzannasir <mramzannasir555@gmail.com>
2024-02-28 13:07:00 +0500

    first commit

---
+++ app/admin/layout.tsx
@@ -0,0 +1,5 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="bg-blue-400">{children}</div>;
+}
